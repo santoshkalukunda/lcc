8e8768bd14d0c5fa09b24908d818bb6bcb047426
santoshkalukunda <santoshkalukunda@gmail.com>
2020-09-15 10:34:49 +0545

    footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -101,7 +101,11 @@
 
         </div>
     </div>
-
+    <footer class="page-footer justify-content-center">
+        <div class="font-13">{{Date('yy')}} © <b>LCC</b> - All rights reserved.  Powered by: <a href="https://mohrain.com/">Mohrain Websoft (P). Ltd.</a> </div>
+       
+        <div class="to-top"><i class="fa fa-angle-double-up"></i></div>
+    </footer>
 </body>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
